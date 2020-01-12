c5cbb3dd35e05379e76f06489f32057bd677ce4a
Lars Schenk <info@lars-schenk.de>
2020-01-12 03:41:05 +0100

    Added sha256.

--- Formula/activebarcodecli.rb
+++ Formula/activebarcodecli.rb
@@ -4,6 +4,7 @@ class Activebarcodecli < Formula
 
 
   url "https://github.com/larsschenk/ActiveBarcodeCLI/archive/v0.0.1.zip", :using => :curl
+  sha256 "2aba1ef633518f4b05456205e21cc0e9ed8c29a88397ba0be2ea5c10fd69f8d0"
 
   def install
     bin.install "schenk/ActiveBarcodeCLI"
